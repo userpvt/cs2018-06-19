b204ad65ea6a81d7593a7a6e3b091bdb2ecc1f98
foxxxyS <vlad2003.vm@gmail.com>
2018-06-21 21:34:22 +0300

    Lesson2 A 2 comlete

--- src/by/it/evs/lesson02/TaskA2.java
+++ src/by/it/evs/lesson02/TaskA2.java
@@ -10,10 +10,6 @@ package by.it.evs.lesson02;
 
 class TaskA2 {
     public static void main(String[] args) {
-        System.out.println("Я начинаю изучать Java!");
-        System.out.println("Я начинаю изучать Java!");
-        System.out.println("Я начинаю изучать Java!");
-        System.out.println("Я начинаю изучать Java!");
-        System.out.println("Я начинаю изучать Java!");
+        for (int i=0;)
     }
 }
