4056696b6ae26d9a6b4ae967ec9540d8be056731
AlexandraTumilovich <darien24688@gmail.com>
2018-06-22 22:44:35 +0300

    lesson1

--- src/by/it/tumilovich/lesson02/TaskB1.java
+++ src/by/it/tumilovich/lesson02/TaskB1.java
@@ -23,7 +23,7 @@ import java.util.Scanner;
 
 class TaskB1 {
     public static void main(String[] args) {
-        System.out.println("Напишите любое число, и я верну вам его квадрат! :)");
+        System.out.println("Напишите любое целое число, и я верну вам его квадрат! :)");
         Scanner scanner=new Scanner(System.in);
         int i=scanner.nextInt();
         System.out.println("Квадрат вашего числа = " + i*i);
