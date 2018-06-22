0dddad71ee22fde60374923cece319e696df48e7
Ivanouski182 <Ivanouski182@gmail.com>
2018-06-22 18:27:17 +0300

    lesson2 a complete

--- src/by/it/ivanouski/lesson02/TaskA2.java
+++ src/by/it/ivanouski/lesson02/TaskA2.java
@@ -11,5 +11,9 @@ package by.it.ivanouski.lesson02;
 class TaskA2 {
     public static void main(String[] args) {
         System.out.println("Я начинаю изучать Java!");
+        System.out.println("Я начинаю изучать Java!");
+        System.out.println("Я начинаю изучать Java!");
+        System.out.println("Я начинаю изучать Java!");
+        System.out.println("Я начинаю изучать Java!");
     }
 }
