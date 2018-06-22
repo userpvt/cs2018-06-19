649fad7330c5148f2589f85c98c523d3a061838b
AlexanderBYN <AlexanderE@tut.by>
2018-06-22 21:31:33 +0300

    A & B

--- src/by/it/evs/lesson02/TaskA2.java
+++ src/by/it/evs/lesson02/TaskA2.java
@@ -10,6 +10,10 @@ package by.it.evs.lesson02;
 
 class TaskA2 {
     public static void main(String[] args) {
-        for (int i=0;)
+        System.out.println("Я начинаю изучать Java!");
+        System.out.println("Я начинаю изучать Java!");
+        System.out.println("Я начинаю изучать Java!");
+        System.out.println("Я начинаю изучать Java!");
+        System.out.println("Я начинаю изучать Java!");
     }
 }
