26ef1b862cc90b956dbae60ba4018577c7512388
AlexandraTumilovich <darien24688@gmail.com>
2018-06-23 00:58:17 +0300

    lesson1

--- src/by/it/tumilovich/lesson02/TaskA2.java
+++ src/by/it/tumilovich/lesson02/TaskA2.java
@@ -10,7 +10,7 @@ package by.it.tumilovich.lesson02;
 
 class TaskA2 {
     public static void main(String[] args) {
-        for( int i =0 ; i < 5; i++){
+        for( int i=0 ; i < 5; i++){
             System.out.println("Я начинаю изучать Java!");
         }
     }
