5951263428e82c05666a607886a9174626e36eff
Loiko <loiko.cvetlana@gmail.com>
2018-06-22 19:13:48 +0200

    Leesson 2 B complite

--- src/by/it/loiko/lesson02/TaskC2.java
+++ src/by/it/loiko/lesson02/TaskC2.java
@@ -1,5 +1,5 @@
 package by.it.loiko.lesson02;
-
+import java.util.Scanner;
 /* Нужно написать программу, которая вводит два числа с клавиатуры
 и 4 раза выводит их сумму с обозначением системы счисления на экран в
 ДЕСЯТИЧНОМ ДВОИЧНОМ ШЕСТНАДЦАТИРИЧНОМ ВОСЬМИРИЧНОМ виде
@@ -15,5 +15,18 @@ OCT:42+32=74
 */
 class TaskC2 {
 
+    public static void main(String[] args) {
 
+        Scanner sc1=new Scanner(System.in);
+        Scanner sc2=new Scanner(System.in);
+
+        int a = sc1.nextInt();
+        int b = sc2.nextInt();
+
+
+        System.out.println("DEC:"  + a + "+" + b + "=" +(a+b ));
+        System.out.println("BIN:"  + Integer.toString(a, 2) + "+" + Integer.toString(b, 2) +  "=" + Integer.toString((a+b), 2));
+        System.out.println("HEX:"  + Integer.toString(a, 16) + "+" + Integer.toString(b, 16) + "=" + Integer.toString((a+b), 16));
+        System.out.println("OCT:"  + Integer.toString(a, 8) + "+" + Integer.toString(b, 8) + "=" + Integer.toString((a+b), 8));
+    }
 }
