251d1179007408802e1e11788110e5e224cb8898
Kuzmichmikhail <Kuzmichmikhail10@gmail.com>
2018-06-22 20:58:07 +0300

    123

--- src/by/it/kuzmich/lesson02/TaskC1.java
+++ src/by/it/kuzmich/lesson02/TaskC1.java
@@ -9,10 +9,11 @@ package by.it.kuzmich.lesson02;
 Вывод (это должна появится в консоли, обратите внимание на пробелы и слово Sum:
 Sum = 26
 */
+    import java.util.Scanner;
 class TaskC1 {
-
-
-
-
-
+        public static void main(String args[]){
+  Scanner scan = new Scanner(System.in);
+    int n1 = scan.nextInt(), n2 = scan.nextInt();
+    int n3 = n1 + n2;
+    System.out.println("Sum =" + " " +n3 );}
 }
