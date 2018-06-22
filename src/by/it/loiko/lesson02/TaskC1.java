68b4785d4a9fb8c81449d20160ce495ca23710bc
Loiko <loiko.cvetlana@gmail.com>
2018-06-22 19:13:48 +0200

    Leesson 2 B complite

--- src/by/it/loiko/lesson02/TaskC1.java
+++ src/by/it/loiko/lesson02/TaskC1.java
@@ -1,4 +1,5 @@
 package by.it.loiko.lesson02;
+import java.util.Scanner;
 
 /* Нужно написать программу, которая вводит два числа с клавиатуры
 и выводит их сумму на экран в виде
@@ -11,8 +12,17 @@ Sum = 26
 */
 class TaskC1 {
 
+    public static void main(String[] args) {
 
+        Scanner sc1=new Scanner(System.in);
+        Scanner sc2=new Scanner(System.in);
 
+        int a = sc1.nextInt();
+        int b = sc2.nextInt();
 
+        System.out.println("Sum = " + (a+b));
+
+    }
 
 }
+
