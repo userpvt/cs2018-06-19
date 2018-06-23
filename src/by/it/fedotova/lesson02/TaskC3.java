fb95b16c7c1c2c5dc6903bd0b0453c3d13099422
helen-fed <fedotovah@gmail.com>
2018-06-23 08:46:39 +0300

    lesson2

--- src/by/it/fedotova/lesson02/TaskC3.java
+++ src/by/it/fedotova/lesson02/TaskC3.java
@@ -1,12 +1,17 @@
 package by.it.fedotova.lesson02;
 
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Scanner;
+
 /*
 Ускорение свободного падения на Земле и Марсе таково:
 Марс   3.86
 Земля   9.81
 
 С клавиатуры вводится вес человека в килограммах.
-Рассчитайте вес человека на Марсе и выведите
+Рассчитайте вес человекана Марсе  и выведите
 округлив его до сотых килограмма (2 знака)
 
 Создайте для этих целей метод getWeight(int weight)
@@ -27,8 +32,28 @@ package by.it.fedotova.lesson02;
 Вывод:
 29.51
 
+// 3.86 *100 / 9.81 = 39.347
+//39.347/100 * 75 = 29.51  0.39
 
 */
 class TaskC3 {
+
+       public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            int k = in.nextInt();
+        }
+
+        public static double getWeight(double weight) {
+
+            double z = 9.81;
+            double y = 3.86;
+            double n = y * 100.0 / z;
+            double k = n / 100.0 * weight;
+
+            double newDouble = new BigDecimal(k).setScale(2, RoundingMode.UP).doubleValue();
+
+            return newDouble;
+
+        }
 
 }
