043e014eb4b6a8e8bae5e0acda576e2e10021232
EroldUpstairs <z.shcharbakou@gmail.com>
2018-06-23 06:41:17 +0300

    lesson2 all done without C3

--- src/by/it/shcharbakou/lesson02/TaskC2.java
+++ src/by/it/shcharbakou/lesson02/TaskC2.java
@@ -13,7 +13,21 @@ BIN:100010+11010=111100
 HEX:22+1a=3c
 OCT:42+32=74
 */
+
+import java.util.Scanner;
+
 class TaskC2 {
 
-
-}
+        public static void main(String args[]) {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Input the 1st num:");
+            int a = in.nextInt();
+            System.out.println("Input the 2nd num:");
+            int b = in.nextInt();
+            int sum = a+b;
+            System.out.println("DEC:"+a+"+"+b+"="+sum);
+            System.out.println("BIN:"+Integer.toBinaryString(a)+"+"+Integer.toBinaryString(b)+"="+Integer.toBinaryString(sum));
+            System.out.println("HEX:"+Integer.toHexString(a)+"+"+Integer.toHexString(b)+"="+Integer.toHexString(sum));
+            System.out.println("OCT:"+Integer.toOctalString(a)+"+"+Integer.toOctalString(b)+"="+Integer.toOctalString(sum));
+        }
+    }
