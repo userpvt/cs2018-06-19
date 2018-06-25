218a2e3cbdccb6fbe1df29fd7730f6676ba62840
AlexanderBYN <AlexanderE@tut.by>
2018-06-25 22:21:38 +0300

    (

--- src/by/it/evs/lesson03/TaskA1.java
+++ src/by/it/evs/lesson03/TaskA1.java
@@ -39,17 +39,17 @@ class TaskA1 {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int sum = a + b;
-        int sub = a - b;
-        int mul = a * b;
-        int div = a / b;
-        int mod = a & b;
-        System.out.println(sum + "" + sub + "" + mul + "" + div + "" + mod);
-        double dsum = a + b;
-        double dsub = a - b;
-        double dmul = a * b;
-        double ddiv = (double) a / b;
-        double dmod = a & b;
-        System.out.println(dsum + "" + dsub + "" + dmul + "" + ddiv + "" + dmod);
+        int sum=a+b;
+        int sub=a-b;
+        int mul=a*b;
+        int div=a/b;
+        int mod=a%b;
+        System.out.println(sum+" "+sub+" "+mul+" "+div+" "+mod);
+        double dsum=a+b;
+        double dsub=a-b;
+        double dmul=a*b;
+        double ddiv=(double)a/b;
+        double dmod=a%b;
+        System.out.println(dsum+" "+dsub+" "+dmul+" "+ddiv+" "+dmod+" ");
     }
 }
--- src/by/it/evs/lesson03/TaskA2.java
+++ src/by/it/evs/lesson03/TaskA2.java
@@ -22,12 +22,12 @@ Lesson 03. Task A2. Стихотворение
 */
 class TaskA2 {
     public static void main(String[] args) {
-        String a = "Лицо в сиянии луны";
-        String b = "Глаза ее всегда грустны";
+        String a = "Лицо в сиянии луны ";
+        String b = "Глаза ее всегда грустны ";
         String c = "Подвижна, трепетна, как лань";
         String d = "И в кольцах  дивных ее длань";
         System.out.println("Мое любимое стихотворение:");
-        System.out.println(abcd);
+        System.out.println(a + "\n" + b + "\n" + c + "\n" + d);
     }
 
 }
