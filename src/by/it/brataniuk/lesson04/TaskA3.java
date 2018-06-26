1f5dcef40934c22c61b47133dd77265e5ae61db5
bratsviat <sviataslau.brataniuk@gmail.com>
2018-06-26 21:03:42 +0300

    lesson4

--- src/by/it/brataniuk/lesson04/TaskA3.java
+++ src/by/it/brataniuk/lesson04/TaskA3.java
@@ -30,13 +30,13 @@ public class TaskA3 {
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
-        if (a == b && b==c)
-          System.out.println(a + " " + b + " " + c + " ");
-            else if (a==b)
-                System.out.println(a + " " + b);
-            else if (b==c)
+        if (a == b && b == c)
+            System.out.println(a + " " + b + " " + c + " ");
+        else if (a == b)
+            System.out.println(a + " " + b);
+        else if (b == c)
             System.out.println(c + " " + b);
-        else if (a==c)
+        else if (a == c)
             System.out.println(a + " " + c);
     }
-    }
+}
