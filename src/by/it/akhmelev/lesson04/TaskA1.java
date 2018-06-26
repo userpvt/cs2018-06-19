e6063c10e2ad3ec2f66bb80c572802fa12dc6d1e
Andrey <valakhanovichdasha@mail.ru>
2018-06-26 21:02:29 +0300

    lesson4 A

--- src/by/it/akhmelev/lesson04/TaskA1.java
+++ src/by/it/akhmelev/lesson04/TaskA1.java
@@ -25,6 +25,15 @@ package by.it.akhmelev.lesson04;
 
  */
 public class TaskA1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i != 3 && i != 9) {
+                System.out.println("Я изучаю программирование.");
+            } else {
+                System.out.println("Меня интересует высокая зарплата!");
+            }
+        }
+    }
 
 
 }
