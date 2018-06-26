bdad5172e6f6f405b5a450dafd296dec56051491
Loiko <loiko.cvetlana@gmail.com>
2018-06-26 21:02:08 +0200

    Lesson 4 a complete

--- src/by/it/loiko/lesson04/TaskA1.java
+++ src/by/it/loiko/lesson04/TaskA1.java
@@ -27,11 +27,15 @@ package by.it.loiko.lesson04;
 public class TaskA1 {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 10 ; i++) {
+        for (int i = 1; i <= 10; i++) {
+            if (i == 3 || i == 9) {
+                System.out.println("Меня интересует высокая зарплата!");
+
+            } else {
+                System.out.println("Я изучаю программирование.");
+            }
 
         }
-
-
 
 
     }
--- src/by/it/loiko/lesson04/TaskA3.java
+++ src/by/it/loiko/lesson04/TaskA3.java
@@ -22,6 +22,31 @@ package by.it.loiko.lesson04;
  */
 
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import java.util.Scanner;
+
 public class TaskA3 {
 
-}
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+
+        if (a == b&& b == c)
+            System.out.println(a + " "+b + " "+c);
+        else if(a==b)
+            System.out.println(a +  " "+b);
+            else if (b ==c)
+                System.out.println(b +" "+c);
+            else if(c ==a)
+            System.out.println(c +" "+a);
+
+
+
+        }
+
+
+    }
+
