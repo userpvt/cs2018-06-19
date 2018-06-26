7e56f8e2a923ad851d8050c2be2b8e22093459ef
EroldUpstairs <z.shcharbakou@gmail.com>
2018-06-26 21:02:52 +0300

    lesson4-A-done

--- src/by/it/shcharbakou/lesson04/TaskA1.java
+++ src/by/it/shcharbakou/lesson04/TaskA1.java
@@ -25,6 +25,14 @@ package by.it.shcharbakou.lesson04;
 
  */
 public class TaskA1 {
-
+    public static void main (String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            if (i != 3 && i != 9)
+                System.out.println("Я изучаю программирование.");
+            else {
+                System.out.println("Меня интересует высокая зарплата!");
+            }
+        }
+    }
 
 }
