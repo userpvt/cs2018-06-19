e52131a4aae4864db40d309506692ba2658f8ff0
Владислав <vladislavcardigans@gmail.com>
2018-06-26 20:15:43 +0300

    lesson3 B complete

--- src/by/it/gavrilenko/lesson02/TaskC2.java
+++ src/by/it/gavrilenko/lesson02/TaskC2.java
@@ -1,5 +1,7 @@
 package by.it.gavrilenko.lesson02;
 
+import java.util.Scanner;
+
 /* Нужно написать программу, которая вводит два числа с клавиатуры
 и 4 раза выводит их сумму с обозначением системы счисления на экран в
 ДЕСЯТИЧНОМ ДВОИЧНОМ ШЕСТНАДЦАТИРИЧНОМ ВОСЬМИРИЧНОМ виде
@@ -14,6 +16,25 @@ HEX:22+1a=3c
 OCT:42+32=74
 */
 class TaskC2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int i=sc.nextInt();
+        int j=sc.nextInt();
+        int sum=i+j;
+        String convert1 = Integer.toBinaryString(i);
+        String convert2 = Integer.toBinaryString(j);
+        String convert3 = Integer.toBinaryString(sum);
+        String convert4 = Integer.toHexString(i).toLowerCase();
+        String convert5 = Integer.toHexString(j).toLowerCase();
+        String convert6 = Integer.toHexString(sum).toLowerCase();
+        String convert7 = Integer.toOctalString(i);
+        String convert8 = Integer.toOctalString(j);
+        String convert9 = Integer.toOctalString(sum);
+        System.out.println("DEC:"+i+"+"+j+"="+sum);
+        System.out.println("BIN:"+convert1+"+"+convert2+"="+convert3);
+        System.out.println("HEX:"+convert4+"+"+convert5+"="+convert6);
+        System.out.println("OCT:"+convert7+"+"+convert8+"="+convert9);
+    }
 
 
 }
--- src/by/it/gavrilenko/lesson03/TaskC1.java
+++ src/by/it/gavrilenko/lesson03/TaskC1.java
@@ -24,7 +24,14 @@ TC = (TF – 32) * 5/9
 в градусы Фаренгейта и возвращать это число.*/
 
 
+import java.util.Scanner;
+
 class TaskC1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int celsium=sc.nextInt();
+        System.out.println(celsium);
+    }
 
 
 
