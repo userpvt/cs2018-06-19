5fb40084b0dd3c0a8ca0e2f0da35bdb38acbd6e6
AlexandraTumilovich <darien24688@gmail.com>
2018-06-26 20:36:43 +0300

    lesson4 Task1

--- src/by/it/tumilovich/lesson04/TaskA1.java
+++ src/by/it/tumilovich/lesson04/TaskA1.java
@@ -25,6 +25,15 @@ package by.it.tumilovich.lesson04;
 
  */
 public class TaskA1 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            if (i!=2&&i!=8){
+                System.out.println("Я изучаю программирование.");
+            } else {
+                System.out.println("Меня интересует высокая зарплата!");
+            }
 
+        }
+    }
 
 }
