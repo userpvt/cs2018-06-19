1d8db6be81dc8460bd4baafc8fd7fef9e4e60772
AlexandraTumilovich <darien24688@gmail.com>
2018-06-26 20:48:28 +0300

    lesson4 Task1

--- src/by/it/tumilovich/lesson04/TaskA2.java
+++ src/by/it/tumilovich/lesson04/TaskA2.java
@@ -24,6 +24,39 @@ package by.it.tumilovich.lesson04;
 
  */
 
-public class TaskA2 {
+import java.util.Scanner;
 
+public class TaskA2 {
+    public static void main(String[] args) {
+        System.out.println("Введите номер дня недели: ");
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        for (int j = 1; j < 8; j++) {
+            switch (j) {
+                case 1:
+                    System.out.println("понедельник");
+                    break;
+                case 2:
+                    System.out.println("вторник");
+                    break;
+                case 3:
+                    System.out.println("среда");
+                    break;
+                case 4:
+                    System.out.println("четверг");
+                    break;
+                case 5:
+                    System.out.println("пятница");
+                    break;
+                case 6:
+                    System.out.println("суббота");
+                    break;
+                case 7:
+                    System.out.println("воскресенье");
+                    break;
+                default:
+                    System.out.println("такого дня недели не существует");
+            }
+        }
+    }
 }
