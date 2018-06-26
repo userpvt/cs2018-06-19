2a9f6623633140c10db33cbb8aa59056a615b8e8
Kuzmichmikhail <Kuzmichmikhail10@gmail.com>
2018-06-26 15:30:23 +0300

    123

--- src/by/it/kuzmich/lesson03/TaskB2.java
+++ src/by/it/kuzmich/lesson03/TaskB2.java
@@ -1,5 +1,7 @@
 package by.it.kuzmich.lesson03;
 
+import java.util.Scanner;
+
 /*
 Lesson 03. Task B2. Нужно написать программу, которая вводит три числа  a b c типа int с клавиатуры
 и вычисляет корни квадратного уравнения a*a*x + b*x + c = 0:
@@ -30,5 +32,38 @@ Lesson 03. Task B2. Нужно написать программу, котора
 
 */
 class TaskB2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        double d = dis(a, b, c);
+        if (d < 0) {
+            System.out.println("Отрицательный дискриминант");
+        } else if (d == 0) {
+            d = (double) Math.sqrt(d);
+            double x = (-b + d) / (2 * a);
+            System.out.println(x);
+        } else {
+            d = (double) Math.sqrt(d);
+            double x1 = (-b + d) / (2 * a);
+            double x2 = (-b - d) / (2 * a);
+            System.out.println(x1 + "" + x2);
+        }
+    }
+        public static double dis(int a, int b, int c) {
+            double d = (double)b* b -4 * a *c;
+            return d;
 
-}
+
+
+        }
+    }
+
+
+
+
+
+
+
+
--- src/by/it/kuzmich/lesson03/TaskC1.java
+++ src/by/it/kuzmich/lesson03/TaskC1.java
@@ -26,6 +26,16 @@ TC = (TF – 32) * 5/9
 
 class TaskC1 {
 
+        public static void main(String[] args) {
+            System.out.println(convertCelsiumToFahrenheit(40));
+        }
+
+        public static double convertCelsiumToFahrenheit(int celsium) {
+            double TF;
+            TF=(double)celsium*9/5+32;
+            return TF;
+        }
+    }
 
 
 
@@ -34,4 +44,4 @@ class TaskC1 {
 //        System.out.println(convertCelsiumToFahrenheit(40));
 //    }
 
-}
+
