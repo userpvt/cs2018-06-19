e1e80dad27d59aa94167b789f6a763d7f8146550
bratsviat <sviataslau.brataniuk@gmail.com>
2018-06-26 14:13:03 +0300

    lesson2

--- src/by/it/brataniuk/lesson03/TaskC2.java
+++ src/by/it/brataniuk/lesson03/TaskC2.java
@@ -20,12 +20,21 @@ package by.it.brataniuk.lesson03;
     5. Метод sumDigitsInNumber должен правильно возвращать сумму всех цифр в числе number.
 */
 public class TaskC2 {
+    public static void main(String[] args) {
+        System.out.println(sumDigitsInNumber(5467));}
 
-
-
-
-//    public static void main(String[] args) {
-//        System.out.println(sumDigitsInNumber(5467));
-//    }
-
+    private static int sumDigitsInNumber(int a){
+    int a1 = a / 1000;
+        //System.out.println(a1);
+    int b1 = (a - a1*1000) / 100;
+        //System.out.println(b1);
+    int c1 = (a-a1*1000-b1*100)/10;
+        //System.out.println(c1);
+    int d1 = a%10;
+        //System.out.println(d1);
+    return (a1+b1+c1+d1);
+    }
 }
+
+
+
