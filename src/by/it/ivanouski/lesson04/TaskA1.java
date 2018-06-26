ebe1229d3588dc5799f73514c82bcd4132cf2165
Ivanouski182 <Ivanouski182@gmail.com>
2018-06-26 21:02:25 +0300

    lesson2 a complete

--- src/by/it/ivanouski/lesson04/TaskA1.java
+++ src/by/it/ivanouski/lesson04/TaskA1.java
@@ -26,6 +26,12 @@ package by.it.ivanouski.lesson04;
  */
 public class TaskA1 {
     public static void main(String[] args) {
-        
+        for (int i = 0; i <= 10; i++) {
+            if (i != 3 && i != 9)
+                System.out.println("Я изучаю программирование.");
+            else
+                System.out.println("Меня интересует высокая зарплата!");
+
+        }
     }
 }
