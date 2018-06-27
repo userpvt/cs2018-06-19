ed8979badf8779bb8fc2fca3b6b9ef3a0519d732
PronovichR4 <pronovich1107@gmail.com>
2018-06-27 20:57:24 +0300

    lesson 4

--- src/by/it/pronovich/lesson04/TaskB1.java
+++ src/by/it/pronovich/lesson04/TaskB1.java
@@ -1,5 +1,7 @@
 package by.it.pronovich.lesson04;
 
+import java.util.Scanner;
+
 /*
 Определите количество дней в году
 
@@ -26,5 +28,25 @@ package by.it.pronovich.lesson04;
 
 */
 public class TaskB1 {
-
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int i=sc.nextInt();
+        year(i);
+    }
+    static void year(int i){
+        if (i%400==0){
+            System.out.println("количество дней в году: 366");
+        }
+        else if (i%100==0 && i%400!=0){
+            System.out.println("количество дней в году: 365");
+        }
+        else if (i%100!=0 && i%4==0){
+            System.out.println("количество дней в году: 366");
+        }
+        else {
+            System.out.println("количество дней в году: 365");
+        }
+    }
 }
+
+
