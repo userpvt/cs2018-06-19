4675153e0626c5db9fab066b87faa5aa3236bc41
baidulena <baiduzhaya@gmail.com>
2018-06-28 18:52:59 +0300

    lessons 4-С2 done

---
+++ src/by/it/baidulena/lesson04/TaskC2.java
@@ -0,0 +1,75 @@
+package by.it.baidulena.lesson04;
+
+import java.util.Scanner;
+
+/*
+
+На входе вводится число и месяц 2018 года.
+Программа должна выводить номер дня года, считая 1 января первым днем,
+а 31 декабря 365-м.
+
+Пример:
+Ввод:
+1 1
+
+Вывод
+1
+
+Ввод:
+31 12
+
+Вывод
+365
+
+Ввод:
+13 9
+
+Вывод
+256
+
+ */
+public class TaskC2 {
+    public static int getMonth(int month) {
+        int a = 31;
+        int b = 30;
+        int c = 28;
+        switch (month) {
+             case 1:
+                return 0;
+            case 3:
+                return a + c;
+            case 5:
+                return a*2 + c + b;
+            case 7:
+                return a*3 + c + b*2;
+            case 8:
+                return a*4 + c + b*2;
+            case 10:
+                return a*5 + c + b*3;
+            case 12:
+                return a*6 + c + b*4;
+            case 2:
+                return c;
+            case 4:
+                return a*2 + c;
+            case 6:
+                return a*3 + c + b;
+            case 9:
+                return a*5 + c + b*2;
+            case 11:
+                return a*6 + c + b*4;
+            default:
+                return 0;
+
+        }
+    }
+    public static void main(String[] ars) {
+        Scanner sc = new Scanner(System.in);
+        int chislo = sc.nextInt();
+        int month = sc.nextInt();
+        System.out.println(getMonth(month) + chislo);
+    }
+}
+
+
+
