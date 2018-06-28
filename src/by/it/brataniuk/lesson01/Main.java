3a0da9549d4aa402c0ee16f105772f2e346065ce
bratsviat <sviataslau.brataniuk@gmail.com>
2018-06-28 14:58:56 +0300

    lesson4

--- src/by/it/brataniuk/lesson01/Main.java
+++ src/by/it/brataniuk/lesson01/Main.java
@@ -7,12 +7,12 @@ public class Main {
             if (binary.length() > 8) binary = binary.substring(binary.length() - 8);
             binary = String.format("%8s", binary).replace(" ", "0");
             String octal = Integer.toOctalString(i);
-            if (octal.length() > 8) octal = octal.substring(octal.length() - 8);
-            octal = String.format("%8s", octal).replace(" ", "0");
+            if (octal.length() > 8) octal = octal.substring(octal.length() - 4);
+            octal = String.format("%4s", octal).replace(" ", "0");
             String hex = Integer.toHexString(i);
-            if (hex.length() > 8) hex = hex.substring(hex.length() - 8);
-            hex = String.format("%8s", hex).replace(" ", "0");
-            System.out.printf("%8s %4s %2s %4d %n", binary, octal, hex, i);
+            if (hex.length() > 8) hex = hex.substring(hex.length() - 2);
+            hex = String.format("%2s", hex).replace(" ", "0");
+            System.out.printf("%8s %4s %2s %2s %n", binary, octal, hex, i);
         }
     }
 }
