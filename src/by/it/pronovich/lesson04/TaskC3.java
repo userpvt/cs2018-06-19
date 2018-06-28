d3469a6a66b6ae7355f284220068811ea16a7bf5
PronovichR4 <pronovich1107@gmail.com>
2018-06-28 18:23:14 +0300

    lesson 4 without c3 complite

--- src/by/it/pronovich/lesson04/TaskC3.java
+++ src/by/it/pronovich/lesson04/TaskC3.java
@@ -24,5 +24,91 @@ package by.it.pronovich.lesson04;
 */
 
 public class TaskC3 {
+    public static void main(String[] args) {
 
+        for (int i = 2; i <= 10; i++) {
+            for (int b = 2; b <= 10; b++) {
+                int y = b * i;
+                System.out.println(intToString(i) + " умножить на " + intToString(b) + " равно " + intToString(y) + " ");
+            }
+        }
+    }
+
+    private static String intToString(int a) {
+        if (a >= 1 && a <= 19) {
+
+            switch (a) {
+                case 1:
+                    return "один";
+                case 2:
+                    return "два";
+                case 3:
+                    return "три";
+                case 4:
+                    return "четыре";
+                case 5:
+                    return "пять";
+                case 6:
+                    return "шесть";
+                case 7:
+                    return "семь";
+                case 8:
+                    return "восемь";
+                case 9:
+                    return "девять";
+                case 10:
+                    return "десять";
+                case 11:
+                    return "одиннадцать";
+                case 12:
+                    return "двенадцать";
+                case 13:
+                    return "тринадцать";
+                case 14:
+                    return "четырнадцать";
+                case 15:
+                    return "пятнадцать";
+                case 16:
+                    return "шестнадцать";
+                case 17:
+                    return "семнадцать";
+                case 18:
+                    return "восемнадцать";
+                case 19:
+                    return "девятнадцать";
+                default:
+                    return " ";
+            }
+
+        } else if (a >= 20 && a <= 99) {
+            switch (a) {
+                case 20:
+                    return "двадцать";
+                case 30:
+                    return "тридцать";
+                case 40:
+                    return "сорок";
+                case 50:
+                    return "пятьдесят";
+                case 60:
+                    return "шестьдесят";
+                case 70:
+                    return "семьдесят";
+                case 80:
+                    return "восемьдесят";
+                case 90:
+                    return "девяносто";
+                default: {
+                    int b = a - a % 10;
+                    int c = a % 10;
+                    return intToString(b) + " " + intToString(c);
+                }
+            }
+
+        } else if (a == 100) {
+            return "сто";
+        } else {
+            return "";
+        }
+    }
 }
