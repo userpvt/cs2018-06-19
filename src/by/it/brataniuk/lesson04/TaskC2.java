2388943b54c6969fa2255cefc3f3526f7e2cf835
bratsviat <sviataslau.brataniuk@gmail.com>
2018-06-28 14:24:03 +0300

    lesson4

--- src/by/it/brataniuk/lesson04/TaskC2.java
+++ src/by/it/brataniuk/lesson04/TaskC2.java
@@ -1,5 +1,7 @@
 package by.it.brataniuk.lesson04;
 
+import java.util.Scanner;
+
 /*
 
 На входе вводится число и месяц 2018 года.
@@ -27,5 +29,25 @@ package by.it.brataniuk.lesson04;
 
  */
 public class TaskC2 {
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int d = sc.nextInt();
+        int m = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i < m; i++) {
+            sum = sum + countdays(i);
+        }
+        System.out.println(sum + d);
+    }
+    public static int countdays(int i) {
+        int day;
+        if (i == 4 || i == 6 || i == 9 || i == 11) {
+            day = 30;
+        } else if (i == 2) {
+            day = 28;
+        } else day = 31;
+        return day;
+    }
 }
+
+
