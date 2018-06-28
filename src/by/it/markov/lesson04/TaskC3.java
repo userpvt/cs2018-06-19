33fdcf79f4204015e49737c12b9c324e718cfbce
isukach <i.sukach.it@gmail.com>
2018-06-28 18:37:00 +0300

    lesson4_f

--- src/by/it/markov/lesson04/TaskC3.java
+++ src/by/it/markov/lesson04/TaskC3.java
@@ -23,37 +23,123 @@ package by.it.markov.lesson04;
 десять умножить на десять равно сто
 */
 
-// public class TaskC3 {
-//    public static void main(String[] args) {
-//
-//    }
-//    public static String printInt(int X) {
-//        String month1;
-//        switch (X) {
-//            case 2: month1 = "два"; break;
-//            case 3: month1 = "три"; break;
-//            case 4: month1 = "четыре"; break;
-//            case 5: month1 = "пять"; break;
-//            case 6: month1 = "шесть"; break;
-//            case 7: month1 = "семь"; break;
-//            case 8: month1 = "восем"; break;
-//            case 9: month1 = "девять";
-//                break;
-//            case 10: month1 = "десять";
-//                break;
-//            case 9: month1 = "сентябрь";
-//                break;
-//            case 10: month1 = "октябрь";
-//                break;
-//            case 11:month1 = "ноябрь";
-//                break;
-//            case 12: month1 = "декабрь";
-//                break;
-//            case 13: month1 = "месяц 13";
-//              break;
-//          default: month1 = "м/яц 14";
-//              break;
-//      }
-//   /  return month1;
-   //
-//
+public class TaskC3 {
+    public static void main(String args[]) {
+        int i;
+        int j;
+        for (i = 2; i <= 10; i++) {
+            String l = getNum(i);
+            for (j = 2; j <= 10; j++) {
+                String m = getNum(j);
+                String n = getNum(i * j);
+                System.out.println(l + " умножить на " + m + " равно " + n);
+            }
+        }
+    }
+
+    public static String getNum(int x) {
+        String Num = "";
+        if ((x > 9 && x < 20) || x == 100) {
+            switch (x) {
+                case 10:
+                    Num = "десять";
+                    break;
+                case 11:
+                    Num = "одиннадцать";
+                    break;
+                case 12:
+                    Num = "двенадцать";
+                    break;
+                case 13:
+                    Num = "тринадцать";
+                    break;
+                case 14:
+                    Num = "четырнадцать";
+                    break;
+                case 15:
+                    Num = "пятнадцать";
+                    break;
+                case 16:
+                    Num = "шестнадцать";
+                    break;
+                case 17:
+                    Num = "семнадцать";
+                    break;
+                case 18:
+                    Num = "восемнадцать";
+                    break;
+                case 19:
+                    Num = "девятнадцать";
+                    break;
+                case 100:
+                    Num = "сто";
+                    break;
+            }
+        } else {
+            int a = x / 10;
+            int b = x % 10;
+            switch (a) {
+                case 0:
+                    Num = "";
+                    break;
+                case 2:
+                    Num = "двадцать ";
+                    break;
+                case 3:
+                    Num = "тридцать ";
+                    break;
+                case 4:
+                    Num = "сорок ";
+                    break;
+                case 5:
+                    Num = "пятьдесят ";
+                    break;
+                case 6:
+                    Num = "шестьдесят ";
+                    break;
+                case 7:
+                    Num = "семьдесят ";
+                    break;
+                case 8:
+                    Num = "восемьдесят ";
+                    break;
+                case 9:
+                    Num = "девяносто ";
+                    break;
+            }
+            switch (b) {
+                case 0:
+                    Num = Num + "\b";
+                    break;
+                case 1:
+                    Num = Num + "один";
+                    break;
+                case 2:
+                    Num = Num + "два";
+                    break;
+                case 3:
+                    Num = Num + "три";
+                    break;
+                case 4:
+                    Num = Num + "четыре";
+                    break;
+                case 5:
+                    Num = Num + "пять";
+                    break;
+                case 6:
+                    Num = Num + "шесть";
+                    break;
+                case 7:
+                    Num = Num + "семь";
+                    break;
+                case 8:
+                    Num = Num + "восемь";
+                    break;
+                case 9:
+                    Num = Num + "девять";
+                    break;
+            }
+        }
+        return Num;
+    }
+}
