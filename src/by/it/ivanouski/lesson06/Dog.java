33a2efab24265ac7ec6dadc2d9de9c06794f5db9
Ivanouski182 <Ivanouski182@gmail.com>
2018-06-30 12:13:16 +0300

    lesson2 a complete

--- src/by/it/ivanouski/lesson06/Dog.java
+++ src/by/it/ivanouski/lesson06/Dog.java
@@ -4,6 +4,8 @@ public class Dog {
 
     private String name;
     private int age;
+    private int weight;
+    private double power;
 
     public Dog() {
     }
@@ -11,6 +13,13 @@ public class Dog {
     public Dog(String name, int age) {
         this.name = name;
         this.age = age;
+    }
+
+    public Dog(String name, int age, int weight, double power) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.power = power;
     }
 
     public String getName() {
@@ -29,8 +38,30 @@ public class Dog {
         this.age = age;
     }
 
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public double getPower() {
+        return power;
+    }
+
+    public void setPower(double power) {
+        this.power = power;
+    }
+
     @Override
     public String toString() {
         return "Кличка: " + name + ". Возраст: " + age;
+    }
+
+    boolean win(Dog otherDog) {
+        double myChance = 0.2 * this.age + 0.3 * this.weight + 0.5 * this.power;
+        double yourChance = 0.2 * otherDog.age + 0.3 * otherDog.weight + 0.5 * otherDog.power;
+        return myChance > yourChance;
     }
 }
