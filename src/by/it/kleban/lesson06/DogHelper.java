35d433cc57392903a2cd2bf9d604ecf08ffadae0
Настя <anastasia.kleban@gmail.com>
2018-06-30 12:21:44 +0300

    pass tests

---
+++ src/by/it/kleban/lesson06/DogHelper.java
@@ -0,0 +1,19 @@
+package by.it.kleban.lesson06;
+
+public class DogHelper {
+    static void printAllNames(Dog[] dogs) {
+        for (Dog dog : dogs) {
+            System.out.println(dog.getName() + "");
+        }
+        System.out.println();
+    }
+
+    static double averageAge(Dog[] dogs){
+        double sum = 0;
+        for (int i = 0; i < dogs.length; i++) {
+            sum = sum + dogs[i].getAge();
+        }
+        return sum / dogs.length;
+
+    }
+}
