9f702c7a6047d9ffb5a84afb0fc89c501516c556
alekseyseth <aleksey.v.sh@gmail.com>
2018-06-30 12:18:50 +0300

    lesson 6 c

---
+++ src/by/it/kuzmich/lesson06/DogHelper.java
@@ -0,0 +1,17 @@
+package by.it.kuzmich.lesson06;
+
+public class DogHelper {
+   static void printAllNames(Dog[] dogs){
+       for (Dog dog : dogs) {
+           System.out.print(dog.getName()+" ");
+       }
+       System.out.println();
+   }
+   static double averageAge(Dog[] dogs){
+       double sum = 0;
+       for (int i = 0; i < dogs.length; i++) {
+           sum = sum+dogs[i].getAge();
+       }
+       return  sum/dogs.length;
+   }
+}
