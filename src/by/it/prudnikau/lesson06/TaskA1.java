76d685d68175e00cc05a6869b73b0e8cb2a60b96
Prudnikov <Lektor_in@mail.ru>
2018-06-30 11:23:17 +0300

    4

--- src/by/it/prudnikau/lesson06/TaskA1.java
+++ src/by/it/prudnikau/lesson06/TaskA1.java
@@ -30,5 +30,15 @@ package by.it.prudnikau.lesson06;
 
 
 public class TaskA1 {
+    public static void main(String[] args) {
+        Dog dog1=new Dog();
+        dog1.setName("Шарик");
+        dog1.setAge(5);
+        Dog dog2=new Dog();
+        dog2.setName("Тузик");
+        dog2.setAge(3);
+        System.out.println(dog1.getName()+" "+dog1.getAge());
+        System.out.println(dog2.getName()+" "+dog2.getAge());
+    }
 
 }
