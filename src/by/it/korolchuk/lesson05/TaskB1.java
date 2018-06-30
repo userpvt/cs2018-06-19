d1597c6d3b0e83e02ba2269e6e1bc896f7f643f5
Kseniya-Korolchuk <kkorolchuk@gmail.com>
2018-06-30 10:52:10 +0300

    lesson 5 A&B

---
+++ src/by/it/korolchuk/lesson05/TaskB1.java
@@ -0,0 +1,39 @@
+package by.it.korolchuk.lesson05;
+/*
+Создайте 5 различных строк в списке ArrayList:
+
+1. Создайте список строк.
+2. Добавьте в него 5 различных строк.
+3. Выведите его размер на экран.
+4. Используя цикл выведите его содержимое на экран, каждое значение с новой строки.
+
+*/
+
+
+import java.util.ArrayList;
+
+public class TaskB1 {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i <5 ; i++) {
+            list.add("zzz" + Integer.toString(i));
+        }
+
+        System.out.println(list.size());
+
+        for (int i = 0; i < list.size(); i++) {      /*первый метод равен второму*/
+
+            System.out.println(list.get(i));
+           /* String str = list.get(i);
+            System.out.println(str);*/
+
+        }
+
+        /*
+        for (String str: list) {
+            System.out.println(str);
+        }*/
+        }
+    }
+
+
---
+++ src/by/it/korolchuk/lesson05/TaskC2.java
@@ -0,0 +1,26 @@
+package by.it.korolchuk.lesson05;
+
+/* Задачка на сортировку
+Задача: Написать программу, которая вводит с клавиатуры 20 чисел и выводит их в убывающем порядке.
+*/
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TaskC2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] m = new int[20];
+        for (int i = 0; i <= m.length - 1; i++) {
+            m[i] = sc.nextInt();
+        }
+
+        //public static void sort (int[] m) {
+
+        Arrays.sort(m);
+
+        for (int i = m.length - 1; i >= 0; i--) {
+            System.out.println(m[i] + "  ");
+        }
+    }
+}
